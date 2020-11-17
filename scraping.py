5609f3cd2b68cb54181d1f157519b4898160ebd0
Alex Felice <golfzander@comcast.net>
2020-11-17 11:31:03 -0600

    Challenge Deliverables 1 and 2

--- scraping.py
+++ scraping.py
@@ -18,7 +18,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": mars_hemispheres(browser)
     }
     # Stop the webdriver and return date
     browser.quit()
@@ -103,7 +104,34 @@ if __name__ == "__main__":
     # If running as script, print scraped data
     print(scrape_all())
 
+def mars_hemispheres(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
 
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+    try:
+        tags = img_soup.find_all('a', class_="itemLink product-item")
+        for tag in tags:
+            if tag.text:
+                hemispheres = {}
+                browser.click_link_by_partial_text("Enhanced")
+                html = browser.html
+                img_soup = soup(html, 'html.parser')   
+                full_img_url = img_soup.find('a', text="Sample")
+                img_descr = img_soup.find('h2', class_='title').get_text()
+                hemispheres['img_url'] = full_img_url['href']
+                hemispheres['title'] = img_descr
+                hemisphere_image_urls.append(hemispheres)   
+    except AttributeError:
+        return None    
+
+    return hemisphere_image_urls
 
 
 
